88b465739880387651ddeb90fa8a247f9fe74f13
benjaminr177 <unoceptik@gmail.com>
2018-04-13 15:50:00 +0300

    meteors dissapear once out of viewport

--- src/app/Game.js
+++ src/app/Game.js
@@ -21,12 +21,41 @@ const generateMeteors = setInterval(() => {
   anime({
     targets: '.meteor',
     translateY: 1250,
-    // opacity: 0.2,
     duration: 7000,
     loop: false,
     elasticity: 0
   })
 }, 2000)
+
+function getComputedTranslateXY(obj)
+{
+	const transArr = [];
+    if(!window.getComputedStyle) return;
+    const style = getComputedStyle(obj),
+        transform = style.transform || style.webkitTransform || style.mozTransform;
+    let mat = transform.match(/^matrix3d\((.+)\)$/);    
+    if(mat) return parseFloat(mat[1].split(', ')[13]);
+    mat = transform.match(/^matrix\((.+)\)$/);
+    mat ? transArr.push(parseFloat(mat[1].split(', ')[4])) : 0;
+    mat ? transArr.push(parseFloat(mat[1].split(', ')[5])) : 0;
+    return transArr;
+}
+
+const removeMeteors = () => {
+  const meteors = document.querySelectorAll('.meteor')
+  const maxHeight = window.innerHeight + 200
+
+  meteors.forEach(meteor => {
+    const currentY = getComputedTranslateXY(meteor)[1]
+    if (currentY > maxHeight) {
+      meteor.remove()
+    }
+  })
+}
+
+setInterval(() => {
+  removeMeteors()
+}, 1000)
 
 const checkIfExploded = setInterval(() => {
   if (Explosion()) {
